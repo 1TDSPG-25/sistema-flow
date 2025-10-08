4213ff7d3fe32df5a4fa7c1dc3cfc3348fa27ed0
yJoaoVictor10 <jvna07@gmail.com>
2025-10-08 18:22:56 -0300

    feat: Adicionando as rotas na const router

--- sistema-flow/src/main.tsx
+++ sistema-flow/src/main.tsx
@@ -2,6 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Login from './routes/Login/index.tsx'
+import Cadastro from './routes/Cadastro/index.tsx'
+
+const router = createBrowserRouter([{
+  path:"/", element:<App/>, children:[
+    {path:"/", element:<Login/>},
+    {path:"/cadastro", element:<Cadastro/>}
+  ]
+}])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
