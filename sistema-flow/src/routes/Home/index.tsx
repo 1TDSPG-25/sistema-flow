708a6eb51d6cf20891fe8c12dfc20dc2fa1de823
enricodelesporte <enricodelesporte@hotmail.com>
2025-10-09 11:28:10 -0300

    feat: adicionando key do post e estilizando  section e h2

--- sistema-flow/src/routes/Home/index.tsx
+++ sistema-flow/src/routes/Home/index.tsx
@@ -1,10 +1,13 @@
 const Home = () => {
     return (
         <main>
-             <section className="">
+             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl w-full p-10 justify-center">
                 {posts.map((post) => (
-                    <article>
-                        
+                    <article 
+                        key={post.id} 
+                        className="bg-slate-800 p-6 rounded-lg shadow-lg border border-slate-700 
+                                   transition-transform duration-300 hover:-translate-y-2">
+                        <h2 className="text-2xl font-bold mb-4">{post.title}</h2>
                     </article>
                 ))}
             </section>
