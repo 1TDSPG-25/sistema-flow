97c8083d54963c8185d6c837f626a9972ea9ebaf
vitordias1006 <vitordias09532@gmail.com>
2025-10-09 11:35:00 -0300

    feat: implement initial post state and update interval in Home component

--- sistema-flow/src/routes/Home/index.tsx
+++ sistema-flow/src/routes/Home/index.tsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+
+    const [posts, setPosts] = useState<string[]>([]);
+
+  useEffect(() => {
+    
+    setPosts(["Primeiro post", "Segundo post"]);
+
+    const interval = setInterval(() => {
+      console.log("Atualizando posts...");
+      setPosts((prev) => [...prev, "Novo post " + new Date().toLocaleTimeString()]);
+    }, 300000); 
+
+    return () => clearInterval(interval);
+  }, []);
+
     return ( <></> );
 }
  
