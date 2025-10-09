8d94d77828cf967e6f41041d74746f7d4d39a828
pedrovaz100 <pedro.vaz.ferreira@gmail.com>
2025-10-09 12:05:54 -0300

    feat: adiciona responsividade no componente Cabecalho

--- sistema-flow/src/components/Cabecalho/Cabecalho.tsx
+++ sistema-flow/src/components/Cabecalho/Cabecalho.tsx
@@ -1,8 +1,11 @@
 export default function Cabecalho() {
-    return (
-        <header>
-            <h1>Sistema Flow</h1>
-            
-        </header>
-    )
+  return (
+    <header className="bg-gray-100 border-b">
+      <div className="mx-auto max-w-screen-lg px-4 py-4 flex flex-col items-center md:flex-row md:justify-between">
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-semibold text-center md:text-left">
+          Sistema Flow
+        </h1>
+      </div>
+    </header>
+  );
 };
