fbbc15dfa26ca6e16d8e1e03801cdef5cf3f3293
iagoliziero <iagolizierox14@gmail.com>
2025-10-09 11:29:43 -0300

    feat: adicionando fetch em Dummy json

--- sistema-flow/src/routes/Home/index.tsx
+++ sistema-flow/src/routes/Home/index.tsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+
+interface apiProps {
+    id: number;
+    quote: string;
+    author: string;
+}
+
 const Home = () => {
+
+    const [posts, setPosts] = useState<apiProps[]>([]);
+
+    const fetchApi = async () => { 
+        const response = await fetch(`${import.meta.env.VITE_API_URL_HOME}`);
+        if(response.status !== 200) {
+            throw new Error('Failed to fetch data');
+        }
+        const data: apiProps[] = await response.json();
+
+        setPosts(data);
+    }
+
+    useEffect(() => {
+
+        fetchApi();
+
+    }, []);
+
     return ( <></> );
 }
  
