7e26dc95c699a0a6fbbabf817ee385230262e160
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-10-09 12:55:08 -0300

    Começando a estilizar a página de login e restauração do rodapé original

--- sistema-flow/src/components/Rodape/Rodape.tsx
+++ sistema-flow/src/components/Rodape/Rodape.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function Rodape(){
     return(
-        <footer>
-            <p>&copy; Todos os direitos reservados.</p>
-        </footer>
+        <nav className="menu flex flex-col md:flex-row items-center justify-center gap-3 md:gap-6 text-base md:text-lg font-medium">
+            <Link to="/" className="px-3 py-1 hover:text-blue-500 transition-colors duration-200">
+                Home
+            </Link>
+            <Link to="/Cadastro" className="px-3 py-1 hover:text-blue-500 transition-colors duration-200">
+                Cadastro
+            </Link>
+            <p>&copy; Todos os direitos reservados</p>
+        </nav>
     );
 }
