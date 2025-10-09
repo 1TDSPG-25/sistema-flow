14fd7714a76cb4e7f3eaba651bc023b9436462e9
JoaooResende <joaovictor231198@gmail.com>
2025-10-09 12:13:30 -0300

    adicionado tailwind no Menu.tsx

--- sistema-flow/src/components/Menu/Menu.tsx
+++ sistema-flow/src/components/Menu/Menu.tsx
@@ -1,8 +1,12 @@
 export default function Menu(){
-    return(
-        <nav className="menu">
-            <Link to="/">Home</Link>
-            <Link to="/Cadastro">Cadastro</Link>
-        </nav>
-    );
+  return(
+    <nav className="menu flex flex-col md:flex-row gap-3 md:gap-6 items-center md:justify-center text-base md:text-lg">
+      <Link to="/" className="px-2 py-1 hover:text-blue-500 transition-colors">
+        Home
+      </Link>
+      <Link to="/Cadastro" className="px-2 py-1 hover:text-blue-500 transition-colors">
+        Cadastro
+      </Link>
+    </nav>
+  );
 }
