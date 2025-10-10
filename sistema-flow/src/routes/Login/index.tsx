b46b54b3ebd3728ca9a0893f02de3d4c42cfa332
LuzBGouveia <lu.barros.gouveia2@gmail.com>
2025-10-10 10:23:16 -0300

    feat: Mudando a estilização da página de login

--- sistema-flow/src/routes/Login/index.tsx
+++ sistema-flow/src/routes/Login/index.tsx
@@ -5,71 +5,142 @@ import { useNavigate } from "react-router-dom";
 const API_URL = import.meta.env.VITE_API_URL_USUARIOS;
 
 export default function Login() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<TipoUser>({
+    mode: "onChange",
+  });
 
-    const {register,handleSubmit,formState:{errors}, reset } = useForm<TipoUser>({
-        mode:"onChange"
-    });
+  const onSubmit = handleSubmit(async (data: TipoUser) => {
+    try {
+      const response = await fetch(API_URL);
+      if (!response.ok) throw new Error("Erro ao buscar usuários");
 
-    const onSubmit = handleSubmit(async (data:TipoUser) => {
+      const usuarios: TipoUser[] = await response.json();
 
-        try {
-            const response = await fetch(API_URL);
-            if(!response.ok) throw new Error("Erro ao buscar usuários");
+      const usuarioValido = usuarios.find(
+        (user) =>
+          user.nome.toLowerCase() === data.nome.toLowerCase() &&
+          user.nomeUser.toLowerCase() === data.nomeUser.toLowerCase() &&
+          user.email === data.email &&
+          user.senha === data.senha
+      );
 
-            const usuarios: TipoUser[] = await response.json();
+      if (usuarioValido) {
+        localStorage.setItem("usuarioLogado", JSON.stringify(usuarioValido));
+        navigate("/home");
+      } else {
+        alert("Credenciais Inválidas.");
+        reset();
+      }
+    } catch (error) {
+      alert("Erro: " + error);
+    }
+  });
 
-            const usuarioValido = usuarios.find(
-                (user) => user.nome.toLowerCase() === data.nome.toLowerCase() && user.nomeUser.toLowerCase() === data.nomeUser.toLowerCase() && user.email === data.email && user.senha === data.senha
-            );
-
-            if(usuarioValido) {
-                localStorage.setItem("usuarioLogado", JSON.stringify(usuarioValido))
-                navigate("/home");
-            } else {
-                alert("Credenciais Inválidas.")
-                reset();
-            }
-
-        } catch (error) {
-            alert("Erro: " + error);
-        }
-    });
-
-    return(
-        <main>
-         <h1  className="text-3xl md:text-4xl font-bold tracking-tight text-center mb-8">Página de Login</h1>
-
-            <div className="mx-auto">
-                <form onSubmit={onSubmit} className="frmLogin space-y-5">
-                    <fieldset>
-                        <legend className= "sr-only">Entrar no sistema</legend>
-                        <div>
-                            <label htmlFor="idNomeUsuario" className="block text-sm font-medium text-gray-700">Nome Completo:</label>
-                            <input type="text" id="idNome" className="bg-amber-200" {...register("nome", { required:"Insira o nome completo", pattern: { value: /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/ , message: "Formato de nome inválido" } })} aria-invalid={!!errors.nome} aria-describedby={errors.nome ? "nome-error" : undefined} /> {errors.nome && <span role="alert" id="nome-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nome.message}</span>}
-                        </div>
-                        <div>
-                            <label htmlFor="idNomeUsuario">Nome Usuário:</label>
-                            <input type="text" id="idNomeUsuario" className="bg-amber-200" {...register("nomeUser", { required:"Insira nome do Usuário" })} aria-invalid={!!errors.nomeUser} aria-describedby={errors.nomeUser ? "nome-user-error" : undefined} /> {errors.nomeUser && <span role="alert" id="nome-user-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.nomeUser.message}</span>}
-                        </div>
-                        <div>
-                            <label htmlFor="idEmail">Email:</label>
-                            <input type="email" id="idEmail" className="bg-amber-200" {...register("email", { required: "Insira email do usuário", pattern: { value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, message: "Formato de email inválido" } })} aria-invalid={!!errors.email} aria-describedby={errors.email ? "email-error" : undefined} /> {errors.email && <span role="alert" id="email-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.email.message}</span>}
-                        </div>
-                        <div>
-                            <label htmlFor="idSenha">Senha:</label>
-                            <input type="text" id="idSenha" className="bg-amber-200" {...register("senha", { required:"Insira a senha do Usuário" })} aria-invalid={!!errors.senha} aria-describedby={errors.senha ? "senha-error" : undefined} /> {errors.senha && <span role="alert" id="senha-error" className="text-red-600 bg-red-300 border-[1px] border-red-600 rounded-md p-2">{errors.senha.message}</span>}
-                        </div>
-                        <div>
-                            <button type="submit">Entrar</button>
-                        </div>
-                        <div>
-                            <p>Caso não tenha um Usuário, clique em <Link to="/cadastro" className="hover:underline hover:text-blue-500 font-bold">Cadastrar-se</Link></p>
-                        </div>
-                    </fieldset>
-                </form>
+  return (
+    <main>
+      <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md mx-auto">
+        <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+          Página de Login
+        </h2>
+        <div className="mx-auto">
+          <form onSubmit={onSubmit} className="space-y-6">
+            <div>
+              <label
+                htmlFor="nome"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Nome Completo
+              </label>
+              <input
+                id="nome"
+                type="text"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                {...register("nome")}
+              />
+              {errors.nome && (
+                <p className="text-red-500 text-sm">{errors.nome.message}</p>
+              )}
             </div>
-        </main>
-    );
+            <div>
+              <label
+                htmlFor="nomeUser"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Nome de Usuário
+              </label>
+              <input
+                id="nomeUser"
+                type="text"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                {...register("nomeUser")}
+              />
+              {errors.nomeUser && (
+                <p className="text-red-500 text-sm">
+                  {errors.nomeUser.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                E-mail
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                {...register("email")}
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm">{errors.email.message}</p>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor="senha"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Senha
+              </label>
+              <input
+                id="senha"
+                type="password"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                {...register("senha")}
+              />
+              {errors.senha && (
+                <p className="text-red-500 text-sm">{errors.senha.message}</p>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Entrar
+            </button>
+            <div>
+              <p className="block text-sm font-medium text-gray-700">
+                Caso não tenha um Usuário, clique em{" "}
+                <Link
+                  to="/cadastro"
+                  className="hover:underline hover:text-blue-500 font-bold"
+                >
+                  Cadastrar
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
 }
