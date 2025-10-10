55e85753173eb66a839f63fc67585559011e545e
PedroBrum-DEV <pbrum191@gmail.com>
2025-10-10 09:25:13 -0300

    Style:responsividade do rodapé

--- sistema-flow/src/components/Rodape/Rodape.tsx
+++ sistema-flow/src/components/Rodape/Rodape.tsx
@@ -1,9 +1,10 @@
 export default function Rodape() {
   return (
-    <footer className="flex flex-col md:flex-row gap-3 md:gap-6 items-center md:justify-center text-base md:text-lg py-4 border-t border-gray-200 mt-6 text-gray-600">
-      <p className="px-2 py-1 hover:text-blue-500 transition-colors">
+    <footer className="flex flex-col sm:flex-row gap-2 sm:gap-4 md:gap-6 items-center justify-center text-sm sm:text-base md:text-lg py-4 px-3 border-t border-gray-200 mt-6 text-gray-600 text-center sm:text-left">
+      <p className="px-2 py-1 hover:text-blue-500 transition-colors duration-300">
         &copy; Todos os direitos reservados - 2025.
       </p>
     </footer>
   );
 }
+
