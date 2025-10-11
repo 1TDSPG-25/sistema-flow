44779cd4073c5b323d65f67ff4b37ea42b47661f
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-11 19:23:49 -0300

    bugFix: Arrumando o alinhamento em Login

--- sistema-flow/src/routes/Login/index.tsx
+++ sistema-flow/src/routes/Login/index.tsx
@@ -49,21 +49,21 @@ export default function LoginForm(){
             <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md mx-auto">
                 <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">Página de Login</h2>
 
-                    <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
-                        <div>
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">E-mail</label>
-                            <input id="email" type="email" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500" {...register("email")} />
-                            {errors.email && (<p className="text-red-500 text-sm">{errors.email.message}</p>)}
-                        </div>
-                        <div>
-                            <label htmlFor="senha" className="block text-sm font-medium text-gray-700">Senha</label>
-                            <input id="senha" type="password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500" {...register("senha")} />
-                            {errors.senha && (<p className="text-red-500 text-sm">{errors.senha.message}</p>)}
-                        </div>
-                        <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">Entrar</button>
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+                    <div>
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">E-mail</label>
+                        <input id="email" type="email" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500" {...register("email")} />
+                        {errors.email && (<p className="text-red-500 text-sm">{errors.email.message}</p>)}
+                    </div>
+                    <div>
+                        <label htmlFor="senha" className="block text-sm font-medium text-gray-700">Senha</label>
+                        <input id="senha" type="password" className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-gray-900 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500" {...register("senha")} />
+                        {errors.senha && (<p className="text-red-500 text-sm">{errors.senha.message}</p>)}
+                    </div>
+                    <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer">Entrar</button>
 
-                        <p className="block text-sm font-medium text-gray-700">Caso não tenha um usuário, clique em <Link to="/cadastro" className="hover:underline hover:text-blue-500 font-bold">Cadastrar</Link></p>
-                    </form>
+                    <p className="block text-sm font-medium text-gray-700">Caso não tenha um usuário, clique em <Link to="/cadastro" className="hover:underline hover:text-blue-500 font-bold">Cadastrar</Link></p>
+                </form>
             </div>
         </main>
     )
