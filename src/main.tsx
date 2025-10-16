76c65742fa68ff102831a078fbe4b0982170fda2
yJoaoVictor10 <jvna07@gmail.com>
2025-10-16 01:00:56 -0300

    feat: rota home adicionada ao main

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,14 @@ import Cadastro from './routes/Cadastro/index.tsx'
 import Produtos from './routes/Produtos/index.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Error404 from './routes/Error/index.tsx'
+import Home from './routes/Home/index.tsx'
 
 const router = createBrowserRouter([{
   path:"/", element:<App/>, errorElement: <Error404/>, children:[
     {path:"/login", element:<Login/>},
     {path:"/cadastro", element:<Cadastro/>},
-    {path:"/produtos", element:<Produtos/>}
+    {path:"/produtos", element:<Produtos/>},
+    {path:"/home", element:<Home/>}
   ]
 }])
 
