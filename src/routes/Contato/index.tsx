c6d1a11a5bc4e68bf9a489f26fa5e2b8dd746dc7
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-23 10:52:19 -0300

    feat(Contato): adicionado os textos do Banner e alterado a url
    Alterado a url da imagem para https://raw.githubusercontent.com/sSofia-s/sistema-flow-assets/refs/heads/main/banner.jpeg

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -2,11 +2,16 @@ export default function Contato() {
   return (
     <section>
       <div className="
-      relative bg-[url('/banner-contato.png')] bg-cover bg-center
+      relative bg-[url('https://raw.githubusercontent.com/sSofia-s/sistema-flow-assets/refs/heads/main/banner.jpeg')] bg-cover bg-center
      text-white py-28 overflow-hidden
     "
       >
-        
+        <div className="relative z-20 flex flex-col items-center gap-7">
+            <h1 className="text-6xl font-bold text-white -mt-10">Contate-nos!</h1>
+            <p className="text-white text-2xl w-[38%] text-center">
+                Precisa de ajuda ou encontrou algum problema? Mande uma mensagem para nós!
+            </p>
+        </div>
       </div>
     </section>
   );
