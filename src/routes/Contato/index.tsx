895fda71081ea49b01235bef6446f2003db3d0d5
Moisés Barsoti <moises.barsot2@gmail.com>
2025-10-23 10:37:35 -0300

    feat(Contato): criado o banner da página
    Ainda vai ser colocado a imagem do repositório

--- src/routes/Contato/index.tsx
+++ src/routes/Contato/index.tsx
@@ -1,7 +1,13 @@
 export default function Contato() {
-    return(
-        <main>
-            <h1>Contato em desenvolvimento.</h1>
-        </main>
-    )
+  return (
+    <section>
+      <div className="
+      relative bg-[url('/banner-contato.png')] bg-cover bg-center
+     text-white py-28 overflow-hidden
+    "
+      >
+        
+      </div>
+    </section>
+  );
 }
