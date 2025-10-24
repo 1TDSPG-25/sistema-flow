0bd575af9d1666eab3428eb85196e6434ab10270
EnzoOkuizumiFiap <enzookuizumifiap@gmail.com>
2025-10-24 09:55:22 -0300

    feat: Implementando na main a função de modo claro e escuro

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Suspense, lazy } from "react";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
+import useTheme from "./context/usetheme";
  
 const Home = lazy(() => import("react-router-dom").then(module => ({ default:module.Outlet})) );
  
+  const { isDark } = useTheme();
  
-const Loading = () => {
-  return <h2>Loading...</h2>;
-};
+  const Loading = () => {
+    return <h2>Loading...</h2>;
+  };
  
 export default function App(){
   return(
-    <div>
+    <div className={`${isDark ? 'dark-mode' : 'light-mode'}`}>
       <Cabecalho />
       {/* Suspense para carregamento NÃO APAGAR e por rotas. Consulte Enrico ou Iago */}
       <Suspense fallback={<Loading />}>
--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,6 +1,8 @@
 import useTheme from "../../context/usetheme";
 import Menu from "../Menu/Menu";
 import { Link } from "react-router-dom";
+import { MdDarkMode as DarkIcon} from "react-icons/md";
+import { MdLightMode as LightIcon} from "react-icons/md";
 
 export default function Cabecalho() {
 
@@ -8,6 +10,7 @@ export default function Cabecalho() {
 
   return (
     <header className={`${isDark ? 'dark-mode' : 'light-mode'}`}>
+      <div><button onClick={toggleTheme}>{isDark ? <LightIcon/> : <DarkIcon/>}</button></div>
       <div className="w-full flex flex-col md:flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-4">
         <h1 className="text-2xl md:text-3xl font-bold tracking-tight mb-2 md:mb-0"><Link to="/">MegaFarma</Link></h1>
         <Menu />
