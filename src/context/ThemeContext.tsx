994f7eb04128928a7bf87ac98143c82341ff937c
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2025-10-24 09:04:17 -0300

    feat: Estrutura base do contexto criada.

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useState, type ReactNode } from "react";
+import type { ThemeContextType } from "../types/themeContextType";
+
+//Criamos o bilhete mágico
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+//Aqui criamos a pessoa que vai dar o bilhete para todo 
+export const ThemeProvider = ({children}:{children : ReactNode })=>{
+
+    //Guardar o estado da luz (se esta escuro ou não);
+    const [isDark, setIsDark] = useState(false);
+
+    //Função para mudar o estado da luz.
+    const toggleTheme = ()=>{
+        setIsDark((prevIsDark)=> !prevIsDark);
+    }
+
+    return(
+        <ThemeContext.Provider value={{isDark, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    );
+
+};
