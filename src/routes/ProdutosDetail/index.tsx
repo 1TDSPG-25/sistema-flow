01317790c61f6b2bbe621c8f7b7a7cf822fcecd3
orlando-IDA <orlandogoncalves8585@gmail.com>
2025-10-30 20:11:12 -0300

    feat(produtos): Adicionado rota para pagina de detalhe de produto e componente basico.

---
+++ src/routes/ProdutosDetail/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProdutosDetail: React.FC = () => {
+    return (
+        <div>
+            <h1>Product Details</h1>
+            <p>Details about the product will go here.</p>
+        </div>
+    );
+};
+
+export default ProdutosDetail;
