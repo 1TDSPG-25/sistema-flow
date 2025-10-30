b78f145cc1c83eb12b396945768298a4410b9cc2
thubrito <britoarthur150@gmail.com>
2025-10-30 15:30:31 -0300

    Recurso(Home): adicionar importação do useTheme para suporte a temas

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import type { apiProps, apiResponse } from "../../types/tipoHome";
+import useTheme from "../../context/useTheme";
 
 export default function Home(){
 const [posts, setPosts] = useState<apiProps[]>([]);
