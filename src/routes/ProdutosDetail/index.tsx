16a8f588f647beba4bc00127b2bd63778858a2a5
orlando-IDA <orlandogoncalves8585@gmail.com>
2025-10-30 20:44:14 -0300

    feat(produto): adiciona states de 'produto' e 'loading'.

--- src/routes/ProdutosDetail/index.tsx
+++ src/routes/ProdutosDetail/index.tsx
@@ -1,7 +1,17 @@
 import { useParams, Link } from "react-router-dom";
+// Importa o useState e useEffect
+import { useState, useEffect } from "react";
+// Importa o tipo do produto (vamos assumir que ele existe em /types)
+import type { TipoProduto } from "../../types/tipoProduto"; 
 
 export default function ProdutoDetail() {
   const { id } = useParams<{ id: string }>();
+
+  const [produto, setProduto] = useState<TipoProduto | null>(null);
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+  }, [id]); 
 
   return (
     <main className="min-h-screen bg-gray-50 text-gray-800">
@@ -13,7 +23,6 @@ export default function ProdutoDetail() {
         <div className="p-8 rounded-xl shadow-md max-w-4xl mx-auto bg-white">
           <h1 className="text-3xl font-bold mb-4">Página de Detalhe do Produto</h1>
           
-          {/* Exibe o ID capturado */}
           <p>Carregando dados para o produto com ID: <strong>{id}</strong></p>
         </div>
       </div>
