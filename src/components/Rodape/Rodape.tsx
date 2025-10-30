6a84802254b51595373666fa5080695d936f79df
thubrito <britoarthur150@gmail.com>
2025-10-30 11:09:06 -0300

    fix(Rodape): atualizando footer para que fixe sempre fixo no seu lugar correto

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -1,13 +1,18 @@
 export default function Rodape() {
   return (
-    <footer className=" inset-x-0 bottom-0 z-50
-                       flex flex-col sm:flex-row gap-2 sm:gap-4 md:gap-6
-                       items-center justify-center text-sm sm:text-base md:text-lg
-                       py-4 px-3 border-t border-gray-200 bg-white/90 backdrop-blur
-                       text-gray-600 text-center sm:text-left">
-      <p className="px-2 py-1 hover:text-blue-500 transition-colors duration-300">
-        &copy; Todos os direitos reservados - 2025.
-      </p>
-    </footer>
+    <>
+      {/* favor, manter este rodapé fixo na parte inferior da página */}
+      <footer
+        className="fixed bottom-0 left-0 w-full z-50
+                   flex flex-col sm:flex-row gap-2 sm:gap-4 md:gap-6
+                   items-center justify-center text-sm sm:text-base md:text-lg
+                   py-4 px-3 border-t border-gray-200 bg-white/90 backdrop-blur
+                   text-gray-600 text-center sm:text-left"
+      >
+        <p className="px-2 py-1 hover:text-blue-500 transition-colors duration-300">
+          &copy; Todos os direitos reservados - 2025.
+        </p>
+      </footer>
+    </>
   );
 }
