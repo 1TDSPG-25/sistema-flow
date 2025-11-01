2dcf8e654e1d691ac85e43f992dfc53d3602c391
orlando-IDA <orlandogoncalves8585@gmail.com>
2025-11-01 18:21:22 -0300

    feat: Adicionado react icons para os links.

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -1,4 +1,5 @@
 import useTheme from "../../context/useTheme";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Rodape() {
   const { isDark } = useTheme();
@@ -22,14 +23,42 @@ export default function Rodape() {
         <p className="px-2 py-1 text-center sm:text-left">
           © {year} Todos os direitos reservados.
         </p>
-        <div className="flex gap-6">
-          <a href="/termos" className="hover:underline">
-            Termos de Uso
-          </a>
-          <a href="/privacidade" className="hover:underline">
-            Privacidade
-          </a>
+        
+        <div className="flex flex-col items-center gap-6 sm:flex-row">
+          <nav>
+            <ul className="flex flex-wrap justify-center gap-x-6 gap-y-2">
+              <li>
+                <a href="/termos" className="hover:underline">
+                  Termos de Uso
+                </a>
+              </li>
+              <li>
+                <a href="/privacidade" className="hover:underline">
+                  Privacidade
+                </a>
+              </li>
+            </ul>
+          </nav>
+          
+          <ul className="flex items-center justify-center gap-5">
+            <li>
+              <a href="https://github.com" target="_blank" className="text-gray-400 hover:text-gray-500">
+                <FaGithub size={20} />
+              </a>
+            </li>
+            <li>
+              <a href="https://linkedin.com" target="_blank" className="text-gray-400 hover:text-gray-500">
+                <FaLinkedin size={20} />
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com" target="_blank" className="text-gray-400 hover:text-gray-500">
+                <FaTwitter size={20} />
+              </a>
+            </li>
+          </ul>
         </div>
+
       </div>
     </footer>
   );
