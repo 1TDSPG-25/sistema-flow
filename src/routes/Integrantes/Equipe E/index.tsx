11a5ffa6e0cfd6dd6b620819d9d42941b6657e7b
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-02 14:25:11 -0300

    feat(Integrantes): feito a base do card 3 da Equipe E e primeira section finalizada.

--- src/routes/Integrantes/Equipe E/index.tsx
+++ src/routes/Integrantes/Equipe E/index.tsx
@@ -69,6 +69,33 @@ export default function EquipeE() {
                 />
               </Link>
             </div>
+
+            {/* Card 3 */}
+            <div className="bg-[#EFD2C3]">
+              <img
+                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762102170/photo-git-user_xq1xkd.png"
+                alt="Foto de um usuário do GitHub."
+              />
+              <h2>Gabriel Martins</h2>
+              <p>Desenvolvedor Back-end</p>
+              <p>Redes Sociais:</p>
+              <Link
+                to="https://github.com/ggabmartins"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
+                  alt="ícone do GitHub"
+                />
+              </Link>
+              <Link to="" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
+                  alt="ícone do LinkedIn"
+                />
+              </Link>
+            </div>
           </div>
         </section>
       </>
