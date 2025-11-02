a589384db389811628ae598aa4a9dc6b4cd0cecd
Moisés Barsoti <moises.barsot2@gmail.com>
2025-11-02 09:15:15 -0300

    feat(Integrantes): criado o conteúdo introdutório
    Adicionado um título e um parágrafo

---
+++ src/routes/Integrantes/index.tsx
@@ -0,0 +1,11 @@
+export default function Integrantes() {
+    return (
+        <section className="flex flex-col gap-15 my-10 px-12">
+            <header className="flex flex-col items-center gap-5">
+                <h1 className="text-6xl font-bold">Conheça nosso time!</h1>
+                <p className="text-3xl">Conheça as equipes que fizeram este projeto acontecer.</p>
+            </header>
+
+        </section>
+    )
+}
