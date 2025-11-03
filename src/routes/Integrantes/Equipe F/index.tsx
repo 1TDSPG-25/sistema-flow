a98a4aefa42ce07bb04cae8257282e3dfc4d73ea
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-02 22:52:31 -0300

    style(Integrantes): responsividade para todos os dispositivos da página Equipe F feita.

--- src/routes/Integrantes/Equipe F/index.tsx
+++ src/routes/Integrantes/Equipe F/index.tsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom";
 export default function EquipeF() {
   return (
     <>
-      <section className="flex flex-col justify-center pl-8">
+      <section className="flex flex-col justify-center pl-8
+       sm:px-8">
         <div className="flex items-center justify-between">
           <h1 className="font-bold text-4xl">Equipe F</h1>
           <Link
@@ -18,17 +19,18 @@ export default function EquipeF() {
         <hr className="border-2 my-3" />
         <p className="text-2xl font-bold">Conheça um pouco mais a Equipe F:</p>
 
-        <ul className="flex flex-col justify-center items-center gap-10 py-10">
+        <ul className="flex flex-col justify-center items-center gap-10 py-10
+        lg:flex-row">
           {/* Card pessoa 1 */}
-          <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
+          <li className="bg-[#DCB2CD] border-[#EF3CB1] rounded-[30px] border-[3px] p-6">
             <img
               src="https://github.com/felipeflosii.png"
               alt="Foto de um usuário do GitHub."
-              className="border-[#433BE5] rounded-[30px] border-[3px] mb-5 w-full"
+              className="border-[#EF3CB1] rounded-[30px] border-[3px] mb-5 w-full"
             />
 
             <h2 className="font-bold text-[32px]">Luiz Felipe</h2>
-            <p className="text-[#433BE5] font-bold text-2xl">
+            <p className="text-[#EF3CB1] font-bold text-2xl">
               Desenvolvedor Back-end
             </p>
             <p className="font-bold text-3xl mt-5 mb-3">Redes Sociais:</p>
@@ -57,14 +59,14 @@ export default function EquipeF() {
           </li>
 
           {/* Card pessoa 2 */}
-          <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
+          <li className="bg-[#DCB2CD] border-[#EF3CB1] rounded-[30px] border-[3px] p-6">
             <img
               src="https://github.com/thubrito.png"
               alt="Foto de um usuário do GitHub."
-              className="border-[#433BE5] rounded-[30px] border-[3px] mb-5"
+              className="border-[#EF3CB1] rounded-[30px] border-[3px] mb-5"
             />
             <h2 className="font-bold text-[32px]">Arthur Brito</h2>
-            <p className="text-[#433BE5] font-bold text-2xl">Desenvolvedor</p>
+            <p className="text-[#EF3CB1] font-bold text-2xl">Desenvolvedor</p>
             <p className="font-bold text-3xl mt-5 mb-3">Redes Sociais:</p>
             <div className="flex gap-7">
               <Link
@@ -87,14 +89,14 @@ export default function EquipeF() {
           </li>
 
           {/* Card pessoa 3 */}
-          <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
+          <li className="bg-[#DCB2CD] border-[#EF3CB1] rounded-[30px] border-[3px] p-6">
             <img
               src="https://github.com/PedroBrum-DEV.png"
               alt="Foto de um usuário do GitHub."
-              className="border-[#433BE5] rounded-[30px] border-[3px] mb-5"
+              className="border-[#EF3CB1] rounded-[30px] border-[3px] mb-5"
             />
             <h2 className="font-bold text-[32px]">Pedro Brum</h2>
-            <p className="text-[#433BE5] font-bold text-2xl">Desenvolvedor</p>
+            <p className="text-[#EF3CB1] font-bold text-2xl">Desenvolvedor</p>
             <p className="font-bold text-3xl mt-5 mb-3">Redes Sociais:</p>
             <div className="flex gap-7">
               <Link
@@ -124,25 +126,36 @@ export default function EquipeF() {
         </h2>
 
         {/* Card tarefa 1 */}
-        <div className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6 mt-10 flex flex-col justify-center items-center w-[90%]">
-          <h2 className="font-bold text-4xl mb-3 text-center">
+        <div className="bg-[#DCB2CD] border-[#EF3CB1] rounded-[30px] border-[3px] p-6 mt-10 flex flex-col justify-center items-center w-[90%]
+        lg:p-8
+        2xl:p-10">
+          <h2 className="font-bold text-4xl mb-5 text-center
+          2xl:text-6xl">
             Componentes principais
         </h2>
-          <p className="text-2xl mb-10">
+          <p className="text-2xl mb-10
+          2xl:text-4xl">
             A equipe foi responsável pelo desenvolvimento das partes principais
             do site, como o Cabeçalho, o Rodapé e o Menu de navegação. Além
             disso, criaram uma página exclusiva para o usuário administrador.
           </p>
           <Link to="/home" target="_blank" rel="noopener noreferrer"
-          className="bg-[#433BE5] rounded-[10px] text-white text-2xl text-center font-bold p-2">
+          className="bg-[#EF3CB1] rounded-[10px] text-white text-2xl text-center font-bold p-2
+          sm:w-[70%]
+          md:w-[55%]
+          lg:w-[40%]
+          2xl:w-[20%]
+          2xl:p-3">
             Clique aqui para ver mais
           </Link>
         </div>
 
         {/* Card tarefa 2 */}
-        <div className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6 mt-10 flex flex-col justify-center items-center w-[90%]">
-          <h2 className="font-bold text-4xl mb-3 text-center">Correções e aperfeiçoamento</h2>
-          <p className="text-2xl">
+        <div className="bg-[#DCB2CD] border-[#EF3CB1] rounded-[30px] border-[3px] p-6 mt-10 flex flex-col justify-center items-center w-[90%]
+        2xl:p-10">
+          <h2 className="font-bold text-4xl mb-3 text-center 2xl:text-6xl">Correções e aperfeiçoamento</h2>
+          <p className="text-2xl
+          2xl:text-4xl">
             Realizaram ajustes na página de produtos e implementaram a rota para
             a lista de produtos. Contribuíram também para o “aperfeiçoamento
             geral” do site, realizando correções e melhorias inesperadas nas
