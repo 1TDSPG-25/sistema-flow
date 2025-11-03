3da68ab3ab7c011139062be82c9104206cd12e73
Sofia Siqueira <ssofiasiq@gmail.com>
2025-11-02 23:21:08 -0300

    feat(Integrantes): feito a base do primeiro e segundo card de tarefas da Equipe G.

--- src/routes/Integrantes/Equipe G/index.tsx
+++ src/routes/Integrantes/Equipe G/index.tsx
@@ -3,116 +3,181 @@ import { Link } from "react-router-dom";
 
 export default function EquipeG() {
   return (
-    <section>
-      <div className="flex items-center justify-between">
-        <h1>Equipe G</h1>
-        <Link
-          to="/integrantes"
-          className="flex font-bold items-center text-xl gap-1"
-        >
-          <IoArrowBackCircleOutline className="size-8" />
-          Voltar
-        </Link>
-      </div>
-      <hr className="border-2 my-3" />
-      <p>Conheça um pouco mais a Equipe G:</p>
+    <>
+      <section>
+        <div className="flex items-center justify-between">
+          <h1 className="font-bold text-4xl">Equipe G</h1>
+          <Link
+            to="/integrantes"
+            className="flex font-bold items-center text-xl gap-1"
+          >
+            <IoArrowBackCircleOutline className="size-8" />
+            Voltar
+          </Link>
+        </div>
+        <hr className="border-2 my-3" />
+        <p className="text-2xl font-bold">Conheça um pouco mais a Equipe G:</p>
 
-      <ul
-        className="flex flex-col justify-center items-center gap-10 py-10"
-      >
-        {/* Card 1 */}
-        <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
-          <img
-            src="https://github.com/moisesBarsoti.png"
-            alt="Foto de um usuário do GitHub."
-            className="border-[#433BE5] rounded-[30px] border-[3px] mb-5 w-full"
-          />
+        <ul className="flex flex-col justify-center items-center gap-10 py-10">
+          {/* Card 1 */}
+          <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
+            <img
+              src="https://github.com/moisesBarsoti.png"
+              alt="Foto de um usuário do GitHub."
+              className="border-[#433BE5] rounded-[30px] border-[3px] mb-5 w-full"
+            />
 
-          <h2>Moisés Barsoti</h2>
-          <p>Desenvolvedor Fullstack</p>
-          <p>Redes Sociais:</p>
-          <div className="flex gap-7">
-            <Link
-              to="https://github.com/orlando-IDA"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <h2 className="font-bold text-[32px]">Moisés Barsoti</h2>
+            <p className="text-[#433BE5] font-bold text-2xl">
+              Desenvolvedor Fullstack
+            </p>
+            <p className="font-bold text-3xl mt-5 mb-3">Redes Sociais:</p>
+            <div className="flex gap-7">
+              <Link
+                to="https://github.com/orlando-IDA"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
+                  alt="ícone do GitHub"
+                />
+              </Link>
+              <Link
+                to="https://github.com/moisesBarsoti"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
+                  alt="ícone do LinkedIn"
+                />
+              </Link>
+            </div>
+          </li>
+
+          {/* Card 2 */}
+          <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
+            <img
+              src="https://github.com/sSofia-s.png"
+              alt="Foto de um usuário do GitHub."
+              className="border-[#433BE5] rounded-[30px] border-[3px] mb-5"
+            />
+            <h2 className="font-bold text-[32px]">Sofia Siqueira</h2>
+            <p className="text-[#433BE5] font-bold text-2xl">Designer UI/UX</p>
+            <p className="font-bold text-3xl mt-5 mb-3">Redes Sociais:</p>
+            <div className="flex gap-7">
+              <Link
+                to="https://github.com/KiuboHIrata"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
+                  alt="ícone do GitHub"
+                />
+              </Link>
+              <Link to="" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
+                  alt="ícone do LinkedIn"
+                />
+              </Link>
+            </div>
+          </li>
+
+          {/* Card 3 */}
+          <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
+            <img
+              src="https://github.com/FeKiModesto.png"
+              alt="Foto de um usuário do GitHub."
+              className="border-[#433BE5] rounded-[30px] border-[3px] mb-5"
+            />
+            <h2 className="font-bold text-[32px]">Felipe Modesto</h2>
+            <p className="text-[#433BE5] font-bold text-2xl">
+              Desenvolvedor Back-end
+            </p>
+            <p className="font-bold text-3xl mt-5 mb-3">Redes Sociais:</p>
+            <div className="flex gap-7">
+              <Link
+                to="https://github.com/ggabmartins"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
+                  alt="ícone do GitHub"
+                />
+              </Link>
+              <Link to="" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
+                  alt="ícone do LinkedIn"
+                />
+              </Link>
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+          <h2>
+            Qual foi o papel da Equipe G durante o projeto?
+          </h2>
+
+          {/* Card tarefa 1 */}
+          <div>
+            <div>
               <img
-                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
-                alt="ícone do GitHub"
-              />
-            </Link>
-            <Link to="https://github.com/moisesBarsoti" target="_blank" rel="noopener noreferrer">
-              <img
-                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
-                alt="ícone do LinkedIn"
-              />
-            </Link>
+                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762136079/pagina-contato_skhd3i.jpg"
+                alt="Foto da tela de contato"
+                />
+              <div>
+                <h2>
+                  Página de Contato
+                </h2>
+                <p>
+                  Desenvolveram um card de informações com telefones, e-mail, e localização.
+                  Com acesso também as Redes Sociais da empresa. Um formulário de contato no qual o
+                  cliente é capaz de enviar suas mensagens.
+                </p>
+                <Link
+                  to="/contato"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Clique aqui para ver mais
+                </Link>
+              </div>
+            </div>
           </div>
-        </li>
 
-        {/* Card 2 */}
-        <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
-          <img
-            src="https://github.com/sSofia-s.png"
-            alt="Foto de um usuário do GitHub."
-            className="border-[#433BE5] rounded-[30px] border-[3px] mb-5"
-          />
-          <h2>Sofia Siqueira</h2>
-          <p>Designer UI/UX</p>
-          <p>Redes Sociais:</p>
-          <div className="flex gap-7">
-            <Link
-              to="https://github.com/KiuboHIrata"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+          {/* Card tarefa 2 */}
+          <div>
+            <div>
               <img
-                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
-                alt="ícone do GitHub"
+                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762136387/pagina-integrantes_bsdh09.jpg"
+                alt="Foto da tela de integrantes"
               />
-            </Link>
-            <Link to="" target="_blank" rel="noopener noreferrer">
-              <img
-                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
-                alt="ícone do LinkedIn"
-              />
-            </Link>
+              <div>
+                <h2>
+                  Página de Integrantes
+                </h2>
+                <p>
+                  Desenvolveram uma página apresentando cada integrante que contribuiu para o projeto,
+                  com a descrição das atividades realizadas por cada equipe.
+                </p>
+                <Link
+                  to="/produtos"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  >
+                  Clique aqui para ver mais
+                </Link>
+              </div>
+            </div>
           </div>
-        </li>
-
-        {/* Card 3 */}
-        <li className="bg-[#B4B2DC] border-[#433BE5] rounded-[30px] border-[3px] p-6">
-          <img
-            src="https://github.com/FeKiModesto.png"
-            alt="Foto de um usuário do GitHub."
-            className="border-[#433BE5] rounded-[30px] border-[3px] mb-5"
-          />
-          <h2>Felipe Modesto</h2>
-          <p>
-            Desenvolvedor Back-end
-          </p>
-          <p>Redes Sociais:</p>
-          <div className="flex gap-7">
-            <Link
-              to="https://github.com/ggabmartins"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103071/icon-github_ep9pkr.png"
-                alt="ícone do GitHub"
-              />
-            </Link>
-            <Link to="" target="_blank" rel="noopener noreferrer">
-              <img
-                src="https://res.cloudinary.com/dt26mfzpw/image/upload/v1762103088/icon-linkedin_p3uaxp.png"
-                alt="ícone do LinkedIn"
-              />
-            </Link>
-          </div>
-        </li>
-      </ul>
-    </section>
+        </section>
+    </>
   );
 }
