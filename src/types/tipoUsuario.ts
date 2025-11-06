0794c755f1d2533bec3a8fd43548cacd90f5d556
PxS00 <lucas.rossonidd@gmail.com>
2025-11-06 20:31:30 -0300

    feat(cadastro): update user data structure to allow null image and maintain formatting consistency

--- src/types/tipoUsuario.ts
+++ src/types/tipoUsuario.ts
@@ -1,8 +1,9 @@
 export type TipoUser = {
-    id: string;
-    nome: string;
-    cpf: string;
-    email: string;
-    senha: string;
-    imagem: string;
-}
+  id: string;
+  nome: string;
+  cpf: string;
+  email: string;
+  senha: string;
+  imagem: string;
+  dataDeNascimento?: string;
+};
